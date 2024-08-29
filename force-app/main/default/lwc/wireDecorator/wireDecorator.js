80e9dc8562acfa882790c5f80586b2292f5abbd6
chmtrip59 <chmtrip59@gmail.com>
2024-08-29 11:48:56 +0530

    newMETHODS in @wire

--- force-app/main/default/lwc/wireDecorator/wireDecorator.js
+++ force-app/main/default/lwc/wireDecorator/wireDecorator.js
@@ -1,12 +1,15 @@
 import {LightningElement,wire, api} from 'lwc';
 
-import {getRecordId,getFieldValue} from 'lightning/uiRecordApi'
+import {getRecord,getFieldValue} from 'lightning/uiRecordApi'
 
 // import NAME_FIELD from '@salesforce/schema/Account.Name';
 // import PHONE_FIELD from '@salesforce/schema/Account.Phone';
 
 
 // Other Method without importing the fields we can directly pass the fields in the wire decorator
+
+const FIELDS =['Account.Name',
+    'Account.Phone'] // To use collection directly in @wire fields:FIELDS to avoid using Account.Name or Account.Phone
 
 
 export default class wireDecorator extends LightningElement{
@@ -18,8 +21,14 @@ export default class wireDecorator extends LightningElement{
 
 //-------------------------OR-----------------------------------------------------
 
-    @wire (getRecordId,{recordId: '$recordId',fields:['Account.Name','Account.Phnoe']})
+    // @wire (getRecord,{recordId: '$recordId',fields:['Account.Name','Account.Phone']})
     
+//--------------------OR-----------------------------
+// OR USING FIELDS COLLECTION
+
+    @wire(getRecord,{recordId: '$recordId',fields:FIELDS})
+
+ 
     
    
    
@@ -34,7 +43,12 @@ export default class wireDecorator extends LightningElement{
 
 //-------------------------------------OR-------------------------------------------------------------------
 
-        return this.record.data ? getFieldValue(this.record.data,'Account.Name'):'';
+        // return this.record.data ? getFieldValue(this.record.data,'Account.Name'):'';
+
+        //OR
+        //without using getFieldValue method 
+
+        return this.record.data.fields.Name.value;
 
     }
     get phone(){
@@ -44,7 +58,14 @@ export default class wireDecorator extends LightningElement{
 
 //------------------------------------------------------OR-------------------------------------------------------------------------------
 
-        return this.record.data ? getFieldValue(this.record.data,'Account.Phone') :'';
+        // return this.record.data ? getFieldValue(this.record.data,'Account.Phone') :'';
+           
+        //OR
+
+        return this.record.data.fields.Phone.value;// to use this we have to find whether our data is present in the record.data or not , for this we have make certain change in the template of the html file to use conditional statement IF 
+        // like <template if:true = {record.data}></template>
+
+        // to check the data is present or not we have to use record.data.fields.fieldName.value
     }
 
 
